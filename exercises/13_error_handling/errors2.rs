a00e7b2108286ba2ce5baf019243b1707b7d3e85
Jabbslad <jabbslad@gmail.com>
2024-07-14 23:20:50 +0100

    Up to quiz 3

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -21,7 +21,10 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
 
     // TODO: Handle the error case as described above.
-    let qty = item_quantity.parse::<i32>();
+    let qty = match item_quantity.parse::<i32>() {
+        Ok(amount) => amount,
+        Err(msg) => return Err(msg),
+    };
 
     Ok(qty * cost_per_item + processing_fee)
 }
